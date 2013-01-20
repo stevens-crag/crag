a9a87cfe3b2ea21dc96fe72b8d9769a63370cdd4
Dmitry Panteleev <dpantel1@stevens.edu>
2013-01-20 18:14:00 -0500

    Unittest framework added

--- common.mk
+++ common.mk
@@ -21,13 +21,11 @@ BIN_DIR = bin
 MAIN_DIR = main
 LIB_DIR = lib
 SRC_DIR = src
+TESTS_DIR = test
 
 ##
 ##  SET SEARCH PATHs
 ##
-
-GSL_PATH = /usr/pkg/lib/
-
 
 vpath %.cpp $(SRC_DIR) $(MAIN_DIR)
 vpath %.o $(OBJ_DIR)
@@ -39,14 +37,14 @@ vpath %.a $(LIB_DIR)
 ##
 ##
 
-CC      = g++   # 
-CFLAGS  = -g -std=c++11 -fpermissive # Compiler flags: -g for debug, -O for optimization
-LDFLAGS = -L$(GSL_PATH) -L $(LIB_DIR)/  $(foreach dir,$(DEPEND_ON),-L../$(dir)/$(LIB_DIR))      # Linker flags
-LIBS    = -lm -lgsl -lgslcblas  -l$(THISLIB) $(foreach name,$(DEPEND_ON),-l$(name)) $(LOCAL_LIBS)
-INCLUDE = -I include/ $(foreach dir,$(DEPEND_ON),-I../$(dir)/include) $(LOCAL_INCLUDE)
+include ../build-config.mk
+
+LDFLAGS += -L $(LIB_DIR)/  $(foreach dir,$(DEPEND_ON),-L../$(dir)/$(LIB_DIR))
+LIBS    += -l$(THISLIB) $(foreach name,$(DEPEND_ON),-l$(name)) $(LOCAL_LIBS)
+INCLUDE += -I include/ $(foreach dir,$(DEPEND_ON),-I../$(dir)/include) $(LOCAL_INCLUDE)
 DEPENDONLIBS = $(DEPEND_ON)
 OBJFILES = $(foreach file,$(SRC),$(OBJ_DIR)/$(file).o)
-
+LIBTESTS = $(wildcard $(TESTS_DIR)/*.cpp)
 
 #############################################################################
 ##
@@ -58,7 +56,7 @@ OBJFILES = $(foreach file,$(SRC),$(OBJ_DIR)/$(file).o)
 #   $? stands for the dependency list (i.e., the .o files)
 #
 
-.PHONY:  all lib  $(THISLIB) lib$(THISLIB)  clean $(MAIN)
+.PHONY:  all lib  $(THISLIB) lib$(THISLIB)  clean $(MAIN) tests check
 
 
 # These are all the files to be compiled.
@@ -68,7 +66,7 @@ lib lib$(THISLIB):	$(LIB_DIR)/lib$(THISLIB).a
 
 ## Make local library
 $(LIB_DIR)/lib$(THISLIB).a : $(OBJFILES)
-	if [ ! -d lib ]; then mkdir lib; fi
+	if [ ! -d $(LIB_DIR) ]; then mkdir $(LIB_DIR); fi
 	$(AR) cr $@ $(OBJFILES)
 	ranlib $@
 
@@ -77,7 +75,7 @@ $(LIB_DIR)/lib$(THISLIB).a : $(OBJFILES)
 $(MAIN) : % :   $(OBJ_DIR)/%.o  $(LIB_DIR)/lib$(THISLIB).a $(DEPENDONLIBS)
 	if [ ! -d $(BIN_DIR) ]; then mkdir -p $(BIN_DIR); fi
 	@echo "$(DEPENDONLIBS)"
-	$(CC) $(CFLAGS) $(INCLUDE) $< $(LDFLAGS) $(LIBS)    -o $(BIN_DIR)/$@
+	$(CXX) $(CPPFLAGS) $(INCLUDE) $(CXXFLAGS) $< $(LDFLAGS) $(LIBS) -o $(BIN_DIR)/$@
 	@echo
 	@echo " ./$@ : compiled sucessfully."
 	@echo
@@ -86,23 +84,66 @@ $(MAIN) : % :   $(OBJ_DIR)/%.o  $(LIB_DIR)/lib$(THISLIB).a $(DEPENDONLIBS)
 $(DEPENDONLIBS): % :
 	cd ../$*; $(MAKE) -f Makefile lib
 
-
 ## compile object files
 $(OBJ_DIR)/%.o : %.cpp
-	${CC} ${CFLAGS} $(INCLUDE) -c $< -o $@
-
+	$(CXX) $(CPPFLAGS) $(INCLUDE) $(CXXFLAGS) -c $< -o $@
 
 ## Build dependencies
 $(OBJ_DIR)/%.d: %.cpp
 	if [ ! -d $(OBJ_DIR) ]; then mkdir -p $(OBJ_DIR); fi
-	$(SHELL) -ec '$(CC) -MM $(INCLUDE) $< | sed "s:$*.o:$(OBJ_DIR)/& $@:g"' > $@
+	$(SHELL) -ec '$(CXX) -MM $(INCLUDE) $< | sed "s:$*.o:$(OBJ_DIR)/& $@:g"' > $@
 
-# Include all the dependency files, this will  automatically remake them if they are
-# out of date
-include $(foreach file,$(SRC) $(MAIN),$(OBJ_DIR)/$(file).d)
 
+# Google test framework integration
+GTEST_DIR = ../gtest
+
+# All Google Test headers.  Usually you shouldn't change this
+# definition.
+GTEST_HEADERS = $(GTEST_DIR)/include/gtest/*.h \
+                $(GTEST_DIR)/include/gtest/internal/*.h
+
+GTEST_SRCS_ = $(GTEST_DIR)/src/*.cc $(GTEST_DIR)/src/*.h $(GTEST_HEADERS)
+
+# For simplicity and to avoid depending on Google Test's
+# implementation details, the dependencies specified below are
+# conservative and not optimized.  This is fine as Google Test
+# compiles fast and for ordinary users its source rarely changes.
+
+$(GTEST_DIR)/gtest-all.o : $(GTEST_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR)/include -I$(GTEST_DIR) $(CXXFLAGS) -c \
+            $(GTEST_DIR)/src/gtest-all.cc -o $(GTEST_DIR)/gtest-all.o
+
+$(GTEST_DIR)/gtest_main.o : $(GTEST_SRCS_)
+	$(CXX) $(CPPFLAGS) -I$(GTEST_DIR)/include -I$(GTEST_DIR) $(CXXFLAGS) -c \
+            $(GTEST_DIR)/src/gtest_main.cc -o $(GTEST_DIR)/gtest_main.o
+
+$(GTEST_DIR)/gtest_main.a : $(GTEST_DIR)/gtest-all.o $(GTEST_DIR)/gtest_main.o
+	$(AR) $(ARFLAGS) $@ $^
+
+## compile object files for unit tests
+$(OBJ_DIR)/unittest_%.o : $(TESTS_DIR)/%.cpp $(GTEST_HEADERS) 
+	$(CXX) $(CPPFLAGS) $(INCLUDE) -I$(GTEST_DIR)/include $(CXXFLAGS) -c $< -o $@
+	
+tests: $(BIN_DIR)/all_unittests
+$(BIN_DIR)/all_unittests: $(LIBTESTS:$(TESTS_DIR)/%.cpp=$(OBJ_DIR)/unittest_%.o) $(GTEST_DIR)/gtest_main.a \
+                      $(LIB_DIR)/lib$(THISLIB).a $(DEPENDONLIBS)
+	@echo "found tests: $(LIBTESTS:$(TESTS_DIR)/%.cpp=%)"
+	@if [ ! -d $(BIN_DIR) ]; then mkdir -p $(BIN_DIR); fi
+	$(CXX) $(CPPFLAGS) $(INCLUDE)-I$(GTEST_DIR)/include $(CXXFLAGS) $^ $(LDFLAGS) -lpthread $(LIBS) -o $@
+	@echo
+	@echo " ./$@ : compiled sucessfully."
+	@echo
+	
+check: tests
+	$(BIN_DIR)/all_unittests
 
 # Clean target to remove backup, object, and core files
 clean:
-	rm -f  $(OBJ_DIR)/*.d  $(OBJ_DIR)/*.o $(LIB_DIR)/*.a
+	rm -f  $(OBJ_DIR)/*.d  $(OBJ_DIR)/*.o $(LIB_DIR)/*.a $(BIN_DIR)/*
+	
+# Include all the dependency files, this will  automatically remake them if they are
+# out of date
+include $(foreach file,$(SRC) $(MAIN),$(OBJ_DIR)/$(file).d)
+	
+	
 
