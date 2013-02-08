72bf823f092e33882cdbcbab8a5327d13a1e4660
Pavel Morar <pvmorar@gmail.com>
2013-02-07 23:22:09 -0500

    Refactoring to use std::map<TerminalSymbol, SLPVertex> instead of SLPSet

--- FreeGroup/include/EndomorphismSLP.h
+++ FreeGroup/include/EndomorphismSLP.h
@@ -5,13 +5,12 @@
  *      Author: pmorar
  */
 
-#ifndef ENDOMORPHISM_SLP_H_
-#define ENDOMORPHISM_SLP_H_
+#ifndef CRAG_FREE_GROUPS_INCLUDE_ENDOMORPHISM_SLP_H_
+#define CRAG_FREE_GROUPS_INCLUDE_ENDOMORPHISM_SLP_H_
 
 #include "SLPSet.h"
 
 namespace crag {
-
 
 
 /**
@@ -24,114 +23,119 @@ public:
 	typedef typename SLPSet::size_type size_type;
 
 	//! Returns the identity automorphism
-	const EndomorphismSLP<rank>& identity() {
-		return _identity;
+	EndomorphismSLP identity() {
+		return EndomorphismSLP();
 	}
 
-	EndomorphismSLP& operator=(const EndomorphismSLP<rank>&) = delete;
-
+	EndomorphismSLP<rank>& operator=(const EndomorphismSLP<rank>& e) {
+		if (this == &e)
+			return *this;
+		images_ = e.images_;
+		return *this;
+	}
 
 	//! Copy constructor
-	explicit EndomorphismSLP(const EndomorphismSLP<rank>& x)
-				: slp_(x.slp_) {}
+	explicit EndomorphismSLP(const EndomorphismSLP<rank>& e)
+				: images_(e.images) {}
 
 	//! Move constructor
-	EndomorphismSLP(EndomorphismSLP<rank>&& x)
-		: slp_(std::move(x.slp_)) {}
+	EndomorphismSLP(EndomorphismSLP<rank>&& e)
+		: images_(std::move(e.images_)) {}
 
-	//! The Nielsen automorphism that inverts the specified terminal and fixes all other terminals
-	EndomorphismSLP(TerminalSymbol inverted_terminal)
-	: slp_(generators_num) {
-		slp_.invert_root(inverted_terminal - 1);
+//	//! The Nielsen automorphism that inverts the specified terminal and fixes all other terminals
+//	EndomorphismSLP(TerminalSymbol inverted_terminal)
+//	: slp_(generators_num) {
+//		slp_.invert_root(inverted_terminal - 1);
+//	}
+//
+//	//! The Nielsen automorphism that maps the specified terminal to the product of another two
+//	EndomorphismSLP(TerminalSymbol mapped_terminal,
+//			SLPVertex left_terminal_vertex,
+//			SLPVertex right_terminal_vertex)
+//	: slp_(generators_num) {
+//		slp_.replace_root(mapped_terminal - 1,
+//						SLPVertex::concatenate(left_terminal_vertex, right_terminal_vertex));
+//	}
+
+//	//! Composes the current automorplhism with the specified Nielsen automorphism
+//	/**
+//	 * Changes the current automorphism so it represents the composition of itself with the given Nielsen
+//	 * automorphism, which inverts the specified terminal symbol.
+//	 *
+//	 * @param inverted_terminal the inverted terminal symbol
+//	 * @return itself after the composition
+//	 */
+//	EndomorphismSLP& composeWithNielsen(TerminalSymbol inverted_terminal) {
+//		slp_.invert_root(inverted_terminal - 1);
+//		return *this;
+//	}
+//
+//	//! Composes the current automorphism with the specified Nielsen automorphism
+//	/**
+//	 * Changes the current automorphism so it represents the composition of itself with the given Nielsen
+//	 * automorphism, which maps the specified terminal to the product of another two.
+//	 *
+//	 * @param mapped_terminal terminal symbol being mapped
+//	 * @param left_terminal_vertex vertex representing left terminal in the product
+//	 * @param right_terminal_vertexlvertex representing right terminal in the product
+//	 * @return itself after the composition
+//	 */
+//	EndomorphismSLP& composeWithNielsen(TerminalSymbol mapped_terminal,
+//			SLPVertex left_terminal_vertex,
+//			SLPVertex right_terminal_vertex) {
+//		auto left = slp_.root(left_terminal_vertex.terminal_symbol() - 1);
+//		if (left_terminal_vertex.is_negative())
+//			left = left.negate();
+//
+//		auto right = slp_.root(right_terminal_vertex.terminal_symbol() - 1);
+//		if (right_terminal_vertex.is_negative())
+//			right = right.negate();
+//
+//		slp_.replace_root(mapped_terminal - 1,
+//				SLPVertex::concatenate(left, right));
+//
+//		return *this;
+//	}
+
+	//! Compose with the given endomorphism.
+	EndomorphismSLP<rank>& operator*=(const EndomorphismSLP<rank>& a);
+
+	//! Compose with the given endomorphism.
+	EndomorphismSLP<rank>& operator*(const EndomorphismSLP<rank>& a);
+
+	//! Returns the image of the terminal.
+	SLPProducedWord image(const TerminalSymbol& t) const {
+		return SLPProducedWord(slp(t));
 	}
 
-	//! The Nielsen automorphism that maps the specified terminal to the product of another two
-	EndomorphismSLP(TerminalSymbol mapped_terminal,
-			SLPVertex left_terminal_vertex,
-			SLPVertex right_terminal_vertex)
-	: slp_(generators_num) {
-		slp_.replace_root(mapped_terminal - 1,
-						SLPVertex::concatenate(left_terminal_vertex, right_terminal_vertex));
-	}
-
-	//! Composes the current automorplhism with the specified Nielsen automorphism
-	/**
-	 * Changes the current automorphism so it represents the composition of itself with the given Nielsen
-	 * automorphism, which inverts the specified terminal symbol.
-	 *
-	 * @param inverted_terminal the inverted terminal symbol
-	 * @return itself after the composition
-	 */
-	EndomorphismSLP& composeWithNielsen(TerminalSymbol inverted_terminal) {
-		slp_.invert_root(inverted_terminal - 1);
-		return *this;
-	}
-
-	//! Composes the current automorphism with the specified Nielsen automorphism
-	/**
-	 * Changes the current automorphism so it represents the composition of itself with the given Nielsen
-	 * automorphism, which maps the specified terminal to the product of another two.
-	 *
-	 * @param mapped_terminal terminal symbol being mapped
-	 * @param left_terminal_vertex vertex representing left terminal in the product
-	 * @param right_terminal_vertexlvertex representing right terminal in the product
-	 * @return itself after the composition
-	 */
-	EndomorphismSLP& composeWithNielsen(TerminalSymbol mapped_terminal,
-			SLPVertex left_terminal_vertex,
-			SLPVertex right_terminal_vertex) {
-		auto left = slp_.root(left_terminal_vertex.terminal_symbol() - 1);
-		if (left_terminal_vertex.is_negative())
-			left = left.negate();
-
-		auto right = slp_.root(right_terminal_vertex.terminal_symbol() - 1);
-		if (right_terminal_vertex.is_negative())
-			right = right.negate();
-
-		slp_.replace_root(mapped_terminal - 1,
-				SLPVertex::concatenate(left, right));
-
-		return *this;
-	}
-
-	//! Applies the given automorphism to the current one and returns the resulting automorphism
-	/**
-	 * Applies the automorphism a to the given one. It constructs a new automorphism and returns it.
-	 * The computational complexity is O(n ln n), where n is the size of the slp representing automorphism a.
-	 */
-	EndomorphismSLP& apply(const EndomorphismSLP<rank>& a);
-
-	//! The image of the terminal t.
-	/**
-	 * @param t terminal (must be between 1 and generators_num())
-	 */
-	SLPProducedWord image(TerminalSymbol t) const {
-		return slp_.produced_word(t - 1);
-	}
-
-	//! The root of SLP representing the image of the terminal t.
-	/**
-	 * @param t terminal (must be between 1 and generators_num())
-	 */
-	SLPVertex slp(TerminalSymbol t) const {
-		return slp_.root(t - 1);
+	//! Returns the root of the straight-line program representing the terminal sybmbol.
+	SLPVertex slp(const TerminalSymbol& t) const {
+		auto result = images_.find(t);
+		if (result == images_.end())
+			return SLPVertex::terminal_vertex(t);
+		else
+			return result->second;
 	}
 
 	//! Returns the generators number of the free group
-	size_type generators_num() const {
+	size_type rank() const {
 		return rank;
 	}
 
 private:
-	EndomorphismSLP(): slp_(rank) {}
-	static const EndomorphismSLP<rank> _identity;
-	SLPSet slp_;//representation
+	EndomorphismSLP() {}//default behavior is fine
+	//! The representation of images of terminal symbols by straight-line programs
+	/**
+	 * If there is no entry for a given terminal symbol, then its image is the terminal itself.
+	 */
+	std::map<TerminalSymbol, SLPVertex> images_;
 
 };
 
 
 template <int rank>
-EndomorphismSLP<rank>& EndomorphismSLP<rank>::apply(const EndomorphismSLP<rank>& a) {//TODO operator *
+EndomorphismSLP<rank>& EndomorphismSLP<rank>::operator*=(const EndomorphismSLP<rank>& a) {
+	//TODO rewirte the method
 	std::unordered_map<SLPVertex, SLPVertex> vertices_map;
 
 	{
@@ -159,7 +163,7 @@ EndomorphismSLP<rank>& EndomorphismSLP<rank>::apply(const EndomorphismSLP<rank>&
 	}
 	//now we have to move the other roots. For them part of the vertices is copied already
 	//so we need to check for this condition
-	for (TerminalSymbol terminal = 2, size = generators_num(); terminal <= size; ++terminal) {
+	for (TerminalSymbol terminal = 2; terminal <= rank; ++terminal) {
 		SLPPostorderInspector inspector(slp(terminal));//TODO make postorder iterator with function that tells when to stop
 		while (!inspector.inspection_ended()) {
 			const SLPVertex& current_vertex = inspector.current_vertex();
@@ -184,9 +188,9 @@ EndomorphismSLP<rank>& EndomorphismSLP<rank>::apply(const EndomorphismSLP<rank>&
 		}
 	}
 	//replacing roots
-	for (TerminalSymbol terminal = 1, n = generators_num(); terminal <= n; ++terminal) {
+	for (TerminalSymbol terminal = 1; terminal <= rank; ++terminal) {
 		auto v_iter = vertices_map.find(slp(terminal));
-		slp_.replace_root(terminal - 1, v_iter->second);
+//		slp_.replace_root(terminal - 1, v_iter->second);
 	}
 	return *this;
 }
@@ -195,4 +199,4 @@ EndomorphismSLP<rank>& EndomorphismSLP<rank>::apply(const EndomorphismSLP<rank>&
 
 
 } /* namespace crag */
-#endif /* ENDOMORPHISM_SLP_H_ */
+#endif /* CRAG_FREE_GROUPS_INCLUDE_ENDOMORPHISM_SLP_H_ */
