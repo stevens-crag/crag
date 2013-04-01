ba18ad5dac006088f50a75121cbb9ffe28c5c8ef
Pavel Morar <pvmorar@gmail.com>
2013-04-01 16:28:53 -0400

    experiments modification

--- FreeGroup/main/endomorphism_slp_tests.cpp
+++ FreeGroup/main/endomorphism_slp_tests.cpp
@@ -6,6 +6,7 @@
  *      Author: pmorar
  */
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include "EndomorphismSLP.h"
 
@@ -46,6 +47,14 @@ class SimpleStat {
       if (val > max_)
         max_ = val;
       ++count_;
+    }
+
+    long long int sum() const {
+      return sum_;
+    }
+
+    unsigned long long int count() const {
+      return count_;
     }
 
     long double average() const {
@@ -110,6 +119,14 @@ class SimpleStat<LongInteger> {
         min_ = max_ = val;
       }
       ++count_;
+    }
+
+    Int sum() const {
+      return sum_;
+    }
+
+    Int count() const {
+      return count_;
     }
 
     Int average() const {
@@ -202,6 +219,10 @@ void enumerate_elementary_automorphisms(int rank, Func f) {
       }
 }
 
+LongInteger total_images_length(const EndomorphismSLP<int>& em) {
+  return get_endomorphism_images_lengths_stat(em).sum();
+}
+
 SimpleStat<LongInteger> image_length_distance(unsigned int rank, const EndomorphismSLP<int>& e1,
                                               const EndomorphismSLP<int>& e2) {
   SimpleStat<LongInteger> length_stat;
@@ -254,43 +275,70 @@ EndomorphismSLP<int> minimize_morphism(unsigned int rank, const EndomorphismSLP<
   return start_morphism;
 }
 
-void conjugation_length_based_attack(unsigned int rank, const EndomorphismSLP<int>& e,
-                                     const EndomorphismSLP<int>& e_conjugated, bool detailed_logging = true) {
+template<typename T>
+struct TargetValues {
+  T morphism_value;
+  T conjugation_value;
+  T minimized_morphism_value;
+  T minimized_conjugation_value;
+
+  TargetValues(T morphism_val, T conjug_val, T min_morphism_val, T min_conjug_val)
+    : morphism_value(morphism_val), conjugation_value(conjug_val),
+      minimized_morphism_value(min_morphism_val), minimized_conjugation_value(min_conjug_val) {}
+
+  static std::string legend() {
+    return "morphism value;conjugation value;minimized morphism value;minimized conjugator value;";
+  }
+};
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const TargetValues<T>& values) {
+  return out << values.morphism_value << ";" << values.conjugation_value << ";"
+                << values.minimized_morphism_value << ";" << values.minimized_conjugation_value << ";";
+}
+
+template<typename TargetFunc, typename TargetValueType = LongInteger>
+TargetValues<TargetValueType> conjugation_length_based_attack(unsigned int rank, const EndomorphismSLP<int>& e,
+                                     const EndomorphismSLP<int>& e_conjugated,
+                                     TargetFunc target_func,
+                                     bool detailed_logging = true) {
+  auto reduced_e = e.free_reduction();
   auto reduced_conj = e_conjugated.free_reduction();
   if (detailed_logging)
     std::cout << "Minimizing conjugation..." << std::endl;
-
-  auto target_func = [&] (const EndomorphismSLP<int>& em) -> LongInteger {
-    return get_endomorphism_images_lengths_stat(em).max();
-//    return image_length_distance(rank, em, e).average();
-  };
 
   auto min = minimize_morphism(rank, reduced_conj, target_func, detailed_logging);
 
   if (detailed_logging)
     std::cout << "Minimizing e itself..." << std::endl;
-  auto min_e = minimize_morphism(rank, e, target_func, detailed_logging);
+  auto min_e = minimize_morphism(rank, reduced_e, target_func, detailed_logging);
 
   if (detailed_logging)
     std::cout << "Comparing the conjugation, e, and minimized versions." << std::endl;
 
   auto reduced_min = min.free_reduction();
   auto reduced_min_e = min_e.free_reduction();
+
+  auto e_val = target_func(reduced_e);
+  auto conj_val = target_func(reduced_conj);
+  auto min_e_val = target_func(min_e);
+  auto min_val = target_func(min);
+
   std::cout << "e :        ";
   print_stats(&std::cout, e);
-  std::cout << std::endl;
+  std::cout << ", val=" << e_val << std::endl;
 
   std::cout << "conj :     ";
   print_stats(&std::cout, reduced_conj);
-  std::cout << std::endl;
+  std::cout << ", val=" << conj_val << std::endl;
 
   std::cout << "min e:     ";
   print_stats(&std::cout, reduced_min_e);
-  std::cout << std::endl;
+  std::cout << ", val=" << min_e_val << std::endl;
 
   std::cout << "min conj : ";
   print_stats(&std::cout, reduced_min);
-  std::cout << std::endl;
+  std::cout << ", val=" << min_val << std::endl;
 
 
 
@@ -305,19 +353,26 @@ void conjugation_length_based_attack(unsigned int rank, const EndomorphismSLP<in
       std::cout << "min   :" << img_min << std::endl;
     }
   }
+
+  return TargetValues<TargetValueType>(e_val, conj_val, min_e_val, min_val);
 }
 
 
 void conjugation_length_based_attack_statistics() {
+  std::ofstream out("lba_based_on_total_length_result.csv");
+  //printing head
+  out << "rank;automorphism size;conjugators num;"
+            << TargetValues<LongInteger>::legend()
+            << std::endl;
   typedef unsigned int uint;
-  for (auto rank : {3, 5}) {
+  for (auto rank : {3, 4, 5}) {
     std::cout << "rank=" << rank << std::endl;
     UniformAutomorphismSLPGenerator<int> rnd(rank);
-    for (auto size : {10, 15}) {
+    for (auto size : {5, 10, 15}) {
       std::cout << "num of composed automorphisms=" << size << std::endl;
-      for (auto conj_num: {10, 15}) {
+      for (auto conj_num: {size}) {
         std::cout << "num of conjugators=" << conj_num << std::endl;
-        const uint iterations_num = 5;
+        const uint iterations_num = 25;
 
         auto start_time = our_clock::now();
         for (int i = 0; i < iterations_num; ++i) {
@@ -326,8 +381,11 @@ void conjugation_length_based_attack_statistics() {
 
           auto e_conjugation = e.conjugate_with(conj_num, rnd);
 
-          conjugation_length_based_attack(rank, e, e_conjugation, false);
-
+          auto result = conjugation_length_based_attack(rank, e, e_conjugation,
+            &total_images_length,
+            false);
+          out << rank << ";" << size << ";" << conj_num << ";"
+                    << result << std::endl;
         }
         auto time_in_ms = std::chrono::duration_cast<std::chrono::milliseconds>(our_clock::now() - start_time);
         std::cout << "(iterations num=" << iterations_num << ",size=" << size << ",conjug num=" << conj_num << ",rank=" << rank << "): "
